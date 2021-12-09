082c914133c75746b9a646414852eec242bf9242
Harsh <mailonithk@gmail.com>
2021-12-09 20:28:27 +0530

    version 2 - crop from gallery fails from camera

--- app/src/main/java/com/example/textrecognizer/MainActivity.java
+++ app/src/main/java/com/example/textrecognizer/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.textrecognizer;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -18,6 +14,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -25,6 +26,10 @@ import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,9 +55,11 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //CropImage.activity().start(MainActivity.this);
+                CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(MainActivity.this);
 
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,101);
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(intent,101);
             }
         });
     }
@@ -60,28 +67,48 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bundle bundle = data.getExtras();
-        Bitmap bitmap = (Bitmap) bundle.get("data");
-        imageView.setImageBitmap(bitmap);
+
+        Uri uri;
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if(resultCode == RESULT_OK){
+                uri = result.getUri();
+                imageView.setImageURI(uri);
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+
+                    imageView.setImageBitmap(bitmap);
 
 
-        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
-        FirebaseVision firebaseVision = FirebaseVision.getInstance();
-        FirebaseVisionTextRecognizer firebaseVisionTextRecognizer = firebaseVision.getOnDeviceTextRecognizer();
-        Task<FirebaseVisionText> task = firebaseVisionTextRecognizer.processImage(image);
+                    FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+                    FirebaseVision firebaseVision = FirebaseVision.getInstance();
+                    FirebaseVisionTextRecognizer firebaseVisionTextRecognizer = firebaseVision.getOnDeviceTextRecognizer();
+                    Task<FirebaseVisionText> task = firebaseVisionTextRecognizer.processImage(image);
 
-        task.addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-            @Override
-            public void onSuccess(FirebaseVisionText firebaseVisionText) {
-                textView.setText(firebaseVisionText.getText());
+                    task.addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                        @Override
+                        public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                            textView.setText(firebaseVisionText.getText());
+                        }
+                    });
+
+                    task.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, "Camera saf kar! /n sarkho photo pad! /n axar sara kadh!", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
             }
-        });
+        }
 
-        task.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(MainActivity.this,"Camera saf kar! /n sarkho photo pad! /n axar sara kadh!",Toast.LENGTH_LONG).show();
-            }
-        });
+//        Bundle bundle = data.getExtras();
+//        Bitmap bitmap = (Bitmap) bundle.get("data");
     }
 }
